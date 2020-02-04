79e70a24a47b023ca6131833a47411af44004346
Joseph Sandmeyer <joey.sandmeyer@gmail.com>
2020-02-03 20:46:01 -0800

    Split sections into two columns of dishes with styled components

---
+++ client/src/components/styled/Column.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Column = styled.div`
+  display: inline-block;
+  vertical-align: top;
+  width: 48%;
+  padding-right: 10px;
+`
+
+export default Column;
