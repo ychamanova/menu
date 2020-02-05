54495a7c2fd4ef2dd8900fdcfd8565a8b80dfa43
Yelena Chamanova <ychamanova@gmail.com>
2020-02-04 19:34:43 -0800

    adjust to server

--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+var cors = require('cors')
 const db = require('../database');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3002;
 
+app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(__dirname + '/../client/dist'));
+app.use(express.static(__dirname + '/../client/dist', {
+  setHeaders: function (res, path) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+  }
+}));
 
 app.get('/api/menu/:pageId', (req, res) => {
   db.getMenuWithPageId(req.params.pageId, (data) => {
@@ -19,6 +24,7 @@ app.get('/api/menu/:pageId', (req, res) => {
 });
 
 app.get('/:pageId', (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
   res.sendFile(path.resolve(__dirname + '/../client/dist/index.html'));
 });
 
